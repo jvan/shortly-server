51afd60b201d20901fd62306d7c301253cc6f6eb
jvan <jordan.vanaalsburg@gmail.com>
2015-11-01 12:04:52 -0800

    Add tests for user api route.

--- test/user_tests.py
+++ test/user_tests.py
@@ -46,3 +46,38 @@ def get_all_users_format_test():
 
     assert_in('id',   user)
     assert_in('name', user)
+
+def get_user_format_test():
+    '''The /users/:user_id route should return valid json.'''
+
+    url = construct_url('users', '1')
+
+    response = requests.get(url)
+
+    # Verify that the call was successful.
+    assert_equal(response.status_code, 200)
+
+    data = response.json()
+
+    # Verify that the respose is ember-data compatible.
+    assert_in('user', data)
+
+    # Verify the format of the user object.
+    user = data['user']
+
+    assert_in('id', user)
+    assert_in('name', user)
+    assert_in('links', user)
+
+    # The links object should be a list of integer ids
+    links = user['links']
+    assert_equal(type(links), list)
+    for link in links:
+	assert_equal(type(link), int)
+
+    # Verify the testing data.
+    assert_equal(user['name'], 'alice@gmail.com')
+
+    assert_equal(len(links), 3)
+    assert_equal(links, [1,2,3])
+    
