0c64c7658349a1ea1a42d0beee1f5c4a519d1d29
jvan <jordan.vanaalsburg@gmail.com>
2015-11-01 11:40:14 -0800

    Add link table and data to database.

--- bin/initialize-database.sh
+++ bin/initialize-database.sh
@@ -19,9 +19,27 @@ if [[ -e "${DATABASE}" ]]; then
   fi
 fi
 
-# Create a table for users and add some test data
+# Create a table for users and add some test data.
 sqlite3 $DATABASE 'CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, username VARCHAR(64) NOT NULL)'
 
 sqlite3 $DATABASE 'INSERT INTO users (username) VALUES ("alice@gmail.com")'
 sqlite3 $DATABASE 'INSERT INTO users (username) VALUES ("bob@gmail.com")'
 sqlite3 $DATABASE 'INSERT INTO users (username) VALUES ("carol@gmail.com")'
+
+
+# Create a table for links and add some test data.
+sqlite3 $DATABASE 'CREATE TABLE links (id INTEGER PRIMARY KEY AUTOINCREMENT, url STRING, user_id INT, FOREIGN KEY (user_id) REFERENCES users(id))'
+
+# Alice likes search engines
+sqlite3 $DATABASE 'INSERT INTO links (url, user_id) VALUES ("http://duckduckgo.com", 1)'
+sqlite3 $DATABASE 'INSERT INTO links (url, user_id) VALUES ("http://google.com", 1)'
+sqlite3 $DATABASE 'INSERT INTO links (url, user_id) VALUES ("http://yahoo.com", 1)'
+
+# Bob works on the backend
+sqlite3 $DATABASE 'INSERT INTO links (url, user_id) VALUES ("http://golang.org", 2)'
+sqlite3 $DATABASE 'INSERT INTO links (url, user_id) VALUES ("http://libstack.com/echo", 2)'
+
+# Carol works on the frontend
+sqlite3 $DATABASE 'INSERT INTO links (url, user_id) VALUES ("http://emberjs.com", 3)'
+sqlite3 $DATABASE 'INSERT INTO links (url, user_id) VALUES ("http://ember-cli.com", 3)'
+sqlite3 $DATABASE 'INSERT INTO links (url, user_id) VALUES ("http://getbootstrap.com", 3)'
